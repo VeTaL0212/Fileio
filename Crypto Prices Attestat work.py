111290ac717d64fb37b3816e3b02d0fcf5cbab1a
Vitali <vitaly.u@mail.ru>
2024-11-25 19:43:57 +0300

    Работает вывод курса bitcoin rub

--- Crypto Prices Attestat work.py
+++ Crypto Prices Attestat work.py
@@ -5,54 +5,43 @@ from tkinter import messagebox as mb
 from tkinter import ttk
 
 
-def update_t_label(event):
-    code = t_combobox.get()
-    name = cur[code]
-    t_label.config(text=name)
+def update_cript_label(event):
+    code = cript_combobox.get()
+    name = cript[code]
+    cript_label.config(text=name)
 
 
-def update_b1_label(event):
-    code = b1_combobox.get()
-    name = cur[code]
-    b1_label.config(text=name)
-
-
-def update_b2_label(event):
-    code = b2_combobox.get()
-    name = cur[code]
-    b2_label.config(text=name)
+def update_currency_label(event):
+    code = currency_combobox.get()
+    name = currency[code]
+    currency_label.config(text=name)
 
 
 def exchange():
-    t_code = t_combobox.get()
-    b1_code = b1_combobox.get()
-    b2_code = b2_combobox.get()
+    cript_code = cript_combobox.get().lower()
+    currency_code = currency_combobox.get().lower()
 
-    if t_code and b1_code and b2_code:
+    if cript_code and currency_code:
         try:
-            response = requests.get(f"https://open.er-api.com/v6/latest/{b1_code}")
-            response2 = requests.get(f"https://open.er-api.com/v6/latest/{b2_code}")# Для получения данных с сервера используется метод `get` библиотеки requests
+            response = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={cript_code}&vs_currencies={currency_code}") # Для получения данных с сервера используется метод `get` библиотеки requests
             response.raise_for_status() # метод `raise_for_status()` в библиотеке requests  Устанавливает статус-код ответа
-            response2.raise_for_status()
             data = response.json() # раскладываем в виде обычного python словаря
-            data2 = response2.json()
-            if t_code in data["rates"]:
-                exchange_rate = data['rates'][t_code]
-                exchange2_rate = data2['rates'][t_code]
-                t_name = cur[t_code]
-                b1_name = cur[b1_code]
-                b2_name = cur[b2_code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {t_name} за один {b1_name} \n и {exchange2_rate:.2f} {t_name} за один {b2_name}")
+
+            if cript_code in data:
+                exchange_rate = data[cript_code][currency_code]
+                cript_name = cript[cript_code]
+                currency_name = currency[currency_code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {currency_name} за один {cript_name}")
             else:
-                mb.showerror("Ошибка", f"Валюта {t_code} не найдена!")
+                mb.showerror("Ошибка", f"Валюта {cript_code} не найдена!")
         except Exception as e:
             mb.showerror("Ошибка", f"Произошла ошибка: {e}.")
     else:
-        mb.showwarning("Внимание", "Введите код калюты")
+        mb.showwarning("Внимание", "Введите код валюты")
 
 
-cur ={
-    'RUB': "Российский рубль",
+currency ={
+    'rub': "Российский рубль",
     'EUR': "Евро",
     'GBP': "Британский фунт стерлингов",
     'JPY': "Японская йена",
@@ -65,31 +54,30 @@ cur ={
     'USD': "Американский доллар"
 }
 
+cript = {
+    "bitcoin": 'BTC',
+    "litecoin": 'LTC'
+}
+
 window = Tk()
-window.title("Курсы обмена валют")
-window.geometry("360x450")
+window.title("Курсы обмена криптовалют")
+window.geometry("360x300")
 
-Label(text="Базавая валюта").pack(padx=10, pady=10)
-b1_combobox = ttk.Combobox(values=list(cur.keys())) # Combobox  виджет в tkinter используется для создания выпадающего списка
-b1_combobox.pack(padx=10, pady=10)
-b1_combobox.bind("<<ComboboxSelected>>", update_b1_label) # bind()  метод tkinter используется для привязки события к виджету
-b1_label = ttk.Label()
-b1_label.pack(padx=10, pady=10)
+Label(text="Базовая валюта").pack(padx=10, pady=10)
+currency_combobox = ttk.Combobox(values=list(currency.keys())) # Combobox  виджет в tkinter используется для создания выпадающего списка
+currency_combobox.pack(padx=10, pady=10)
+currency_combobox.bind("<<ComboboxSelected>>", update_currency_label) # bind()  метод tkinter используется для привязки события к виджету
+currency_label = ttk.Label()
+currency_label.pack(padx=10, pady=10) # метод `pack()` в tkinter размещает виджеты в окне
 
-Label(text="Вторая базавая валюта").pack(padx=10, pady=10)
-b2_combobox = ttk.Combobox(values=list(cur.keys()))
-b2_combobox.pack(padx=10, pady=10)
-b2_combobox.bind("<<ComboboxSelected>>", update_b2_label)
-b2_label = ttk.Label()
-b2_label.pack(padx=10, pady=10)
+Label(text="Криптовалюта").pack(padx=10, pady=10)
 
+cript_combobox = ttk.Combobox(values=list(cript.keys()))
+cript_combobox.pack(padx=10, pady=10)
+cript_combobox.bind("<<ComboboxSelected>>", update_cript_label)
 
-Label(text="Целевая валюта").pack(padx=10, pady=10)
-t_combobox = ttk.Combobox(values=list(cur.keys()))
-t_combobox.pack(padx=10, pady=10)
-t_combobox.bind("<<ComboboxSelected>>", update_t_label)
-t_label = ttk.Label()
-t_label.pack(padx=10, pady=10)
+cript_label = ttk.Label()
+cript_label.pack(padx=10, pady=10)
 
 Button(text="Получить курс обмена", command=exchange).pack(padx=10, pady=10)
 
